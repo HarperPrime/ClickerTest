90dd73abc72c58abeb2727757a136d49d499dbdc
HarperPrime <jlibke@gmail.com>
2018-04-18 22:28:23 -0500

    test
    test

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour {
+
+    public Text woodDisp;
+    public Text stoneDisp;
+    public Text metalDisp;
+
+    public float woodTotal = 0;
+    public float stoneTotal = 0;
+    public float metalTotal = 0;
+
+    public float woodClick = 1;
+    public float stoneClick = 1;
+    public float metalClick = 1;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        woodDisp.text = woodTotal.ToString();
+        stoneDisp.text = stoneTotal.ToString();
+        metalDisp.text = metalTotal.ToString();
+   
+	}
+}
---
+++ Assets/Manual_Gather.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Manual_Gather : MonoBehaviour {
+
+    public GameManager GM;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // add wood when clicking on the Gather Wood button
+    public void woodClicked()
+    {
+        GM.woodTotal += GM.woodClick;
+    }
+    
+    // add stone when clicking on the Gather Stone button
+    public void stoneClicked()
+    {
+        GM.stoneTotal += GM.stoneClick;
+    }
+
+    // add metal when clicking on the Gather Metal button
+    public void metalClicked()
+    {
+        GM.metalTotal += GM.metalClick;
+    }
+}
